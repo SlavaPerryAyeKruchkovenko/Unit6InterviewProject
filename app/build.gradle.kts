ba65464a08a584ef27efc20a93628bee5ec8a791
Slava <kruchkovenko@gmail.com>
2024-05-23 18:27:47 +0500

    update libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment)
-    implementation(libs.compose.preview.renderer)
+    implementation(libs.converter.gson)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/unit6/interviewproject/MainFragment.kt
+++ app/src/main/java/com/unit6/interviewproject/MainFragment.kt
@@ -1,10 +1,14 @@
 package com.unit6.interviewproject
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 
 /**
  * A simple [Fragment] subclass.
@@ -17,8 +21,16 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        test()
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false)
 
+    }
+    fun test() = runBlocking {
+        Log.d("EXAMPLE","Hello from ${Thread.currentThread().name}")
+        withContext(Dispatchers.Default) {
+            Log.d("EXAMPLE","Hello from ${Thread.currentThread().name}")
+        }
+        Log.d("EXAMPLE","Welcome back to ${Thread.currentThread().name}")
     }
 }
